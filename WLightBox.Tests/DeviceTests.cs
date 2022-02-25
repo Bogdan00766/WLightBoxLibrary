b044fdb357ff64f579e462044771c23c488f2a5b
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2022-02-25 15:48:45 +0100

    minor changes

---
+++ WLightBox.Tests/DeviceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using WLightBox.Library;
+
+namespace WLightBox.Tests
+{
+    internal class DeviceTests
+    {
+        [Test]
+        void IsConnected_Connected_ReturnTrue()
+        {
+            //var mockMyPing = new Mock<Ping>();
+            //mockMyPing.Setup(x => x.Send("1.1.1.1")).Returns();
+            //var mockMyHttpClient = new Mock<MyHttpClient>();
+            //var Device = new Device("1.1.1.1", mockMyPing.Object); 
+        }
+    }
+}
